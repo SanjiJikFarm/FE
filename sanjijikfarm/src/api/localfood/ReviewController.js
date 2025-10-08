6f48582af21852fa0f729b66ee0c2c62f6eca331
only1Ksy <winterksy@gmail.com>
2025-10-08 21:23:21 +0900

    fix: url to params to fit with api structure

--- sanjijikfarm/src/api/localfood/ReviewController.js
+++ sanjijikfarm/src/api/localfood/ReviewController.js
@@ -4,6 +4,8 @@ import { withErrorBoundary } from '../axios/axios';
 /** shop id를 넘기면 매장의 리뷰 목록을 반환하는 함수 */
 export const getShopReviewList = (shopId) =>
   withErrorBoundary(async () => {
-    const res = await axiosInstance.get(`/shops/${shopId}/reviews`);
+    const res = await axiosInstance.get(`/reviews`, {
+      params: { shopId: shopId },
+    });
     return res.data;
   });

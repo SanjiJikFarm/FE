f2576c4b59a8034356bf0ae0ea2313c8b5b32f1b
only1Ksy <winterksy@gmail.com>
2025-10-18 01:26:35 +0900

    feat: add skeleton to home decrease amount

--- sanjijikfarm/src/components/feature/home/MainDecreaseAmount.jsx
+++ sanjijikfarm/src/components/feature/home/MainDecreaseAmount.jsx
@@ -11,7 +11,24 @@ export default function MainDecreaseAmount({ month }) {
     staleTime: 1000 * 60,
   });
 
-  if (isLoading || error) return;
+  if (isLoading) {
+    return (
+      <div className="flex h-45 w-full p-4">
+        <div className="border-main-green flex h-full w-full animate-pulse flex-col items-center justify-center gap-2 rounded-3xl border-2 bg-gray-100 p-7">
+          <div className="flex items-center gap-3">
+            <div className="bg-gray-3 h-6 w-40 rounded-md"></div>
+            <div className="bg-gray-3 h-6 w-6 rounded-full"></div>
+          </div>
+          <div className="flex items-end gap-1">
+            <div className="bg-gray-3 h-10 w-24 rounded-md"></div>
+            <div className="bg-gray-3 h-8 w-12 rounded-md"></div>
+          </div>
+        </div>
+      </div>
+    );
+  }
+
+  if (error) return <div>error</div>;
 
   return (
     <div className="flex h-45 w-full p-4">
